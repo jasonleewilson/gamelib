3f63e026c525eab2b4398279455b826baa6ae133
Jason Lee Wilson <robotici@yahoo.com>
2025-05-10 22:20:23 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -19,11 +19,9 @@ function App() {
           <a href='https://dev.gamelib-d40.pages.dev'>
             Take a peek at my staging branch
           </a>
-          https://staging.gamelib-d40.pages.dev
           <a href='https://staging.gamelib-d40.pages.dev'>
             Take a peek at my dev branch
           </a>
-          https://staging.gamelib-d40.pages.dev
         </div>
       </div>
     </>
